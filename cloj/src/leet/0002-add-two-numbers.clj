6287852e5d37688a5e8415e2756a1e66d782dac2
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2021-06-29 15:33:28 -0600

    Update 0002-add-two-numbers.clj

--- cloj/src/leet/0002-add-two-numbers.clj
+++ cloj/src/leet/0002-add-two-numbers.clj
@@ -18,3 +18,21 @@
 (= [7 0 8] (f1 [2 4 3] [5 6 4])) ;; => true
 (= [0] (f1 [0] [0])) ;; => true
 (= [8 9 9 9 0 0 0 1] (f1 [9 9 9 9 9 9 9] [9 9 9 9])) ;; => true
+
+;; 2
+(defn zip-it [l]
+  (apply +
+         (map #(* %1 %2)
+              l
+              (map #(Math/pow 10 %) (range)))))
+(defn unzip-it [n]
+  (->> n
+       str
+       reverse
+       (map #(Integer. (str %)))))
+(defn f2 [l1 l2]
+  (unzip-it
+   (apply +
+          (map #(-> % zip-it int) [l1 l2]))))
+(f2 [2 4 3] [5 6 4])
+(f2 [9 9 9 9 9 9 9] [9 9 9 9])
