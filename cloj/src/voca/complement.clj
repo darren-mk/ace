d12c356a35b830794715c7287cfb8509a9952f7f
Minsoo Kim <minsookim@Minsoos-MacBook-Pro.local>
2022-04-08 06:32:44 -0400

    implemented core functions

--- cloj/src/voca/complement.clj
+++ cloj/src/voca/complement.clj
@@ -1,24 +1,13 @@
-(comment
-  (defn complement
-    "Takes a fn f and returns a fn that takes the same arguments as f,
-  has the same effects, if any, and returns the opposite truth value."
-    {:added "1.0"
-     :static true}
-    [f] 
-    (fn 
-      ([] (not (f)))
-      ([x] (not (f x)))
-      ([x y] (not (f x y)))
-      ([x y & zs] (not (apply f x y zs))))))
+((complement <) 3 2) ;; => true
+(filter (complement odd?) [1 2 3 4]) ;; => (2 4)
+((complement even?) 1) ;; => true
+(#(not (even? %)) 1) ;; => true
 
-((complement <) 3 2)
-;; => true
-
-(filter (complement odd?) [1 2 3 4])
-;; => (2 4)
-
-((complement even?) 1)
-;; => true
-
-(#(not (even? %)) 1)
-;; => true
+;; implementation
+;; a
+(defn complement-a [f]
+  (fn [& x] (not (apply f x))))
+((complement-a <) 3 2) ;; => true
+(filter (complement-a odd?) [1 2 3 4]) ;; => (2 4)
+((complement-a even?) 1) ;; => true
+(#(not (even? %)) 1) ;; => true
