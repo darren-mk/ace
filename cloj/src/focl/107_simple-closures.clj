3b1b9546d06d5b12c19f5da59bcd8ef14d94f473
Darren Minsoo Kim <darren.minsoo.kim@gmail.com>
2022-05-08 09:01:19 -0400

    practiced simple closures

--- cloj/src/focl/107_simple-closures.clj
+++ cloj/src/focl/107_simple-closures.clj
@@ -1,12 +1,4 @@
-;;https://www.4clojure.com/problem/107
-
-;; pow
-(Math/pow 7 2) ;; => 49.0
-;; int
-(int 3.3) ;; => 3
-(int 4.9) ;; => 4
-(int -0.8) ;; => 0
-(int -1.1) ;; => -1
+;; https://4clojure.oxal.org/#/problem/107
 
 ;; 1
 (defn f1 [a]
@@ -21,3 +13,19 @@
 (= 256 ((f2 2) 16) ((f2 8) 2)) ;; => true 
 (= [1 8 27 64] (map (f2 3) [1 2 3 4])) ;; => true
 (= [1 2 4 8 16] (map #((f2 %) 2) [0 1 2 3 4])) ;; => true
+
+;; 3
+(defn pow
+  ([a]
+   (* a a))
+  ([a b]
+   (pow a b 1))
+  ([a b result]
+   (if (<= b 0)
+     result
+     (pow a (dec b) (* a result)))))
+(defn f3 [x]
+  (fn [y] (pow y x)))
+(= 256 ((f3 2) 16) ((f3 8) 2)) ;; => true 
+(= [1 8 27 64] (map (f3 3) [1 2 3 4])) ;; => true
+(= [1 2 4 8 16] (map #((f3 %) 2) [0 1 2 3 4])) ;; => true
