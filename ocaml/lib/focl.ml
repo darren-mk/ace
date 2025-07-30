8cfd8b3c9a37fd2ae4feaca8d6d115155a6801c9
Darren Kim <darren.minsoo.kim@gmail.com>
2025-07-30 06:39:07 -0400

    set up env for ocaml

---
+++ ocaml/lib/focl.ml
@@ -0,0 +1,11 @@
+(* Problem 1, Nothing but the Truth
+   Difficulty: elementary
+   Complete the expression so it will evaluate to true.
+   (= __ true) *)
+
+let f x = not x
+let _ = f true = false
+
+let f_001_a b = not b
+
+let () = print_endline "Hello, World!"
